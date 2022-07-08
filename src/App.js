c2417a6281feacac04a6e8e419d24f4e2b953cb7
sheapa <patshea26@gmail.com>
2022-07-08 13:53:22 -0400

    complete project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,66 @@
+import React, {useState} from 'react';
+
 import Expenses from './Components/Expenses/Expenses';
 import NewExpense from './Components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2',
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) 
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2022, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Azure Subscription',
+    amount: 800,
+    date: new Date(2023, 5, 12),
+  },
+  {
+    id: 'e6',
+    title: 'Amazon Subscription',
+    amount: 150,
+    date: new Date(2024, 5, 12),
+  },
+  {
+    id: 'e7',
+    title: 'IRS',
+    amount: 1800,
+    date: new Date(2025, 5, 12),
+  },
+  {
+    id: 'e8',
+    title: 'AWS',
+    amount: 500,
+    date: new Date(2026, 5, 12),
+  }
+];
+
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...expenses];
+    });
   };
 
   return (
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpenseChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -12,32 +13,18 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <div>
+    <li>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpenseChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses}/>
       </Card>
-    </div>
+    </li>
   );
 };
 
